87c8e443ce715c13806452a0839af0ae06703e37
Ruslan04071996 <ruslan040796@mail.ru>
2020-08-11 11:46:47 +0300

    Работает все без стилей

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ const USER_PER_PAGE = 5;
 const URL = 'https://api.github.com/';
 const reposList = document.querySelector('.repositories-js');
 const mainContainer = document.querySelector('.main-js');
-
-
-
-
-
 async function loadRepos() {
     return await fetch(`${URL}search/repositories?q=${input.value}&per_page=${USER_PER_PAGE}`)
         .then(res => {
@@ -18,13 +13,14 @@ async function loadRepos() {
             console.log(res['items']);
             const resArr = res['items'];
             resArr.forEach(el => {
+
                 const elementreposList = createElement('li'); //Нужно добавить класс для стилизации 
                 elementreposList.textContent = el.name;
                 reposList.append(elementreposList);
 
                 function clickHandler() {
 
-                    const container = createElement('div');
+                    const container = createElement('div','container');
                     reposList.style.display = 'none';
                     mainContainer.append(container);
                     const spanName = createElement('div');
@@ -37,18 +33,21 @@ async function loadRepos() {
                     container.append(spanOwner);
                     container.append(spanStars);
                 }
-
                 elementreposList.addEventListener('click', clickHandler);
+
+
             })
-
-
-
         })
-
 }
 
-loadRepos = debounce(loadRepos,500);
+loadRepos = debounce(loadRepos, 500);
 input.addEventListener('keyup', loadRepos);
+input.addEventListener('keyup', () => {
+    if (!input.value) {
+        reposList.innerHTML = '';
+        document.querySelector('.container').innerHTML = '';
+    }
+})
 function createElement(elementName, className) {
     const element = document.createElement(elementName);
     if (className) {
@@ -66,6 +65,3 @@ function debounce(fn, ms) {
     }
 }
 
-function clearUsers() {
-
-}
